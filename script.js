3130fe3e24a4f38dd35bbaaed0c566b7d97176cd
Le Gloux Thomas <legloux.thomas@gmail.com>
2023-01-10 14:16:12 +0100

    html, css and js files added

---
+++ script.js
@@ -0,0 +1,6 @@
+const clickBtn = document.getElementById('confirmBtn');
+const clickConfirm = document.getElementById('clickConfirm');
+
+clickBtn.addEventListener("click", clickConfirm.document.innerHTML = "Vous avez cliqué sur le bouton !");
+
+
